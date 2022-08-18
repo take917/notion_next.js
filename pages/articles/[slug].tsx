b796bf054346cd0874ab8de498bef8856f995f93
take917 <1tktk2tk3@gmail.com>
2022-08-18 22:30:40 +0900

    🎨 make page

---
+++ pages/articles/[slug].tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import ArticleMeta from "../../components/ArticleMeta";
+import Layout from "../../components/Layout";
+
+const Article = () => {
+  return (
+    <Layout>
+      <article className="w-full">
+        {/* meta section */}
+        <div className="my-12">
+          <ArticleMeta />
+        </div>
+
+        {/* article */}
+        <div className="my-12">article</div>
+      </article>
+    </Layout>
+  );
+};
+
+export default Article;
