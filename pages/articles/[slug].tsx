1f116760082a01e92b1618381484ac493ddd50a0
take917 <1tktk2tk3@gmail.com>
2022-08-20 23:38:35 +0900

    🎨 make components

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,19 +1,34 @@
+import { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
 import React from "react";
 import ArticleMeta from "../../components/ArticleMeta";
 import Layout from "../../components/Layout";
+import { ArticleProps, Params } from "../../types/types";
+import { sampleCards } from "../../utils/sample";
 
-const Article = () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { slug } = ctx.params as Params;
+
+  const page = sampleCards.find((data) => data.slug === slug);
+
+  return {
+    props: {
+      page: page,
+    },
+  };
+};
+
+const Article: NextPage<ArticleProps> = ({ page }) => {
   return (
     <Layout>
       <article className="w-full">
         {/* meta section */}
         <div className="my-12">
-          <ArticleMeta />
+          <ArticleMeta page={page} />
         </div>
 
         {/* article */}
-        <div className="my-12">article</div>
+        <div className="my-12">article {page.content}</div>
       </article>
     </Layout>
   );
